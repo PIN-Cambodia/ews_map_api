95dafd00d9b09fd3f3f4064c9ff31beca2f18c5b
chendaloeurt <chenda.loeurt@gmail.com>
2019-04-03 16:57:07 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ews1294_map
+
+
+Developed for new EWS front page. Uses Leaflet Javascript library for mapping, and eCharts for charting data.
+
+Data is stored in sensorLocations.js and echart\sensorRecentData in GeoJSON/JSON format. Code could be adapted to read directly from database/wfs/geoserver.
+
+For more info on leaflet check out; http://leafletjs.com/ and http://echarts.baidu.com/feature.html
+
---
+++ sensorLocations.js
@@ -0,0 +1,187 @@
+
+var sensorData1 = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    104.9265,
+                    11.54294
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Phnom Penh",
+                "id": 23,
+                "name": "Phnom Penh",
+                "namekh": "",
+                "external_id": "TEPv2.2-015",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    104.9262,
+                    11.54294
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Phnom Penh",
+                "id": 24,
+                "name": "Phnom Penh",
+                "namekh": "",
+                "external_id": "TEPv2.2-003",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    106.01862,
+                    12.47984
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Kratie",
+                "id": 25,
+                "name": "Kratie",
+                "namekh": "",
+                "external_id": "TEPv2.2-013",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    104.9256,
+                    11.54294
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Phnom Penh",
+                "id": 26,
+                "name": "Phnom Penh",
+                "namekh": "",
+                "external_id": "TEPv2.2-004",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    104.1631,
+                    10.67728
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Kampot",
+                "id": 27,
+                "name": "Kampot",
+                "namekh": "",
+                "external_id": "TEPv2.2-011",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        },
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    104.05975,
+                    11.1938
+                ]
+            },
+            "properties": {
+                "marker": "#FF0000",
+                "popup-text": "Sihanoun",
+                "id": 28,
+                "name": "Sihanoun",
+                "namekh": "",
+                "external_id": "TEPv2.2-008",
+                "sensor_height": null,
+                "distance_report": null,
+                "water_height": null,
+                "status": "Test",
+                "type": null,
+                "comment": "",
+                "commentkh": "",
+                "trigger_levels": {
+                    "severe_warning": 0,
+                    "warning": 0,
+                    "watch_level": 0
+                }
+            }
+        }
+    ]
+};
+
